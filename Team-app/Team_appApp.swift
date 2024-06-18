f0196bb61bc2cb5a657b96f2d2b29e1d6f0ff553
Hisa12 <hisago.52612@gmail.com>
2024-06-18 13:19:05 +1000

    Initial Commit

---
+++ Team-app/Team_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team_appApp.swift
+//  Team-app
+//
+//  Created by Hisano Sato on 18/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Team_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
